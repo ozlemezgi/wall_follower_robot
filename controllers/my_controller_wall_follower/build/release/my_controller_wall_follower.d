a500582229346bd95a64dcad5a76fd5299eb55ad
ozlemezgi <ozlemezgisari@icloud.com>
2022-01-14 13:55:45 +0200

    first commit

---
+++ controllers/my_controller_wall_follower/build/release/my_controller_wall_follower.d
@@ -0,0 +1,7 @@
+build/release/my_controller_wall_follower.o: \
+ my_controller_wall_follower.cpp \
+ /snap/webots/20/usr/share/webots/include/controller/cpp/webots/Robot.hpp \
+ /snap/webots/20/usr/share/webots/include/controller/cpp/webots/Motor.hpp \
+ /snap/webots/20/usr/share/webots/include/controller/cpp/webots/Device.hpp \
+ /snap/webots/20/usr/share/webots/include/controller/cpp/webots/../../c/webots/types.h \
+ /snap/webots/20/usr/share/webots/include/controller/cpp/webots/DistanceSensor.hpp
